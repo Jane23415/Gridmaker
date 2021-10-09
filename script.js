0ee150c9e8de9f7b58a0578254c650d0387120c8
markallman51 <markallman51@gmail.com>
2021-10-09 17:19:10 -0400

    add row works, also started add col. will finish in its own branch

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
 let numRows = 0;
 let numCols = 0;
-let colorSelected; 
+let colorSelected;
 
-//Add a row
+
+
+//Add a row(rows are tr tag)
 function addR() {
-    alert("Clicked Add Row")
+    let table = document.getElementById("grid")
+    let col = document.createElement("td")
+    let row = document.createElement("tr")
+    
+    //if there's no cols, append 1 tr with 1 td child(one box)
+    if(numCols === 0){
+        console.log("no cols")
+        row.appendChild(document.createElement("td"))  
+        table.appendChild(row)
+        console.log(row.childNodes)
+
+        if(numRows === 0)//if there are no rows or cols, adding one box will create a 1x1 grid, so updare cols
+            numCols++
+    }
+    
+    //if there are cols, append 1 td to the new row for each col
+    else{
+        console.log(numCols, " cols")
+        for(let i = 0; i < numCols; i++)
+            row.appendChild(document.createElement("td"))
+            
+        table.appendChild(row) 
+    }
+
+    numRows++
+    //console.log(numRows)
 }
-//Add a column
+//Add a column(columns are td tag)
 function addC() {
-    alert("Clicked Add Col")
+    let table = document.getElementById("grid")
+    let col = document.createElement("td")
+    let row = document.createElement("tr")
+    
+    if(numRows === 0){
+        console.log("no cols")
+        row.appendChild(document.createElement("td"))  
+        table.appendChild(row)
+        console.log(row.childNodes)
+        
+        if(numCols === 0)//if there are no rows or cols, adding one box will create a 1x1 grid, so update row
+        numRows++
+    }
+    
+    numCols++
 }
 
 //Remove a row
